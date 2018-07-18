828dc57c7f50ed5b13fd35913fc8a08a3ecb190b
Gaspare <ferraro.gaspare.majo@gmail.com>
2018-07-18 15:07:41 +0200

    review #1

---
+++ script/index.js
@@ -0,0 +1,6 @@
+$(function(){
+     d3.csv("data/authors.csv").then(data => $("#authors_count").text(data.length));
+     d3.csv("data/papers.csv").then(data => $("#papers_count").text(data.length));
+     d3.csv("data/journals.csv").then(data => $("#journals_count").text(data.length));
+     d3.csv("data/coauthorship.csv").then(data => $("#coauthorship_count").text(data.length));
+});
